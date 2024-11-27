fffb952ffb34d08979c073ad32ce0bf3cb1040b7
xiaomengxin <xibaole2333@gmail.com>
2024-11-28 01:40:28 +0800

    update cli flags

--- cmd/spotdl/cmd.go
+++ cmd/spotdl/cmd.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"fmt"
-	"os"
-
 	log "github.com/XiaoMengXinX/spotdl/logger"
 	"github.com/XiaoMengXinX/spotdl/spotify"
 	"github.com/spf13/pflag"
+	"os"
+	"path/filepath"
 )
 
 func main() {
 	var (
 		showHelp           = pflag.BoolP("help", "h", false, "Show this help message")
 		id                 = pflag.StringP("id", "i", "", "ID/URL/URI of a spotify track/playlist/album/podcast to download (Required)\nExample: -i https://open.spotify.com/track/4jTrKMoc44RYZsoFsIlQev")
-		quality            = pflag.StringP("quality", "q", "", "Audio quality level. (default \"MP4_128_DUAL\")\nOptions: MP4_128, MP4_128_DUAL, MP4_256, MP4_256_DUAL")
-		output             = pflag.StringP("output", "o", "./output", "Output directory for downloaded files")
-		config             = pflag.StringP("config", "c", "config.json", "Path to configuration file")
+		quality            = pflag.StringP("quality", "q", "", "Audio quality level (default \"MP4_128_DUAL\")\nOptions: MP4_128, MP4_128_DUAL, MP4_256, MP4_256_DUAL")
+		output             = pflag.StringP("output", "o", "./", "Output directory for downloaded files")
+		config             = pflag.StringP("config", "c", "", "Path to configuration file")
 		debug              = pflag.BoolP("debug", "d", false, "Debug mode")
 		convertToMP3       = pflag.BoolP("mp3", "", false, "Convert downloaded files to mp3 format")
 		skipAddingMetadata = pflag.BoolP("no-metadata", "", false, "Skip adding metadata to downloaded files")
@@ -28,12 +28,27 @@ func main() {
 		os.Exit(0)
 	}
 	if *id == "" {
-		fmt.Println("Error: -id is required")
-		pflag.Usage()
+		fmt.Printf("Usage: %s -i <spotify_id_or_url> [options]\n", os.Args[0])
+		fmt.Println("Use -h or --help for more information")
 		os.Exit(1)
 	}
 	if *debug {
 		log.SetLevel(log.LevelDebug)
+	}
+
+	if *config == "" {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			*config = "config.json"
+		} else {
+			configDir := filepath.Join(homeDir, ".config", "spotdl")
+			if err := os.MkdirAll(configDir, 0755); err != nil {
+				log.Warnf("Failed to create config directory: %v", err)
+				*config = "config.json"
+			} else {
+				*config = filepath.Join(configDir, "config.json")
+			}
+		}
 	}
 
 	sp := spotify.NewDownloader()
