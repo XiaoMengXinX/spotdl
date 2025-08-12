af80bff0e8952c39c035c4a5e335f2bd50cb7a8f
XiaoMengXinX <xibaole2333@gmail.com>
2025-08-13 01:41:52 +0800

    increase timeout for injector to 120s

--- injector/injector.go
+++ injector/injector.go
@@ -40,7 +40,7 @@ type interceptStatus struct {
 func DefaultOptions() *InterceptOptions {
 	return &InterceptOptions{
 		Headless:     true,
-		Timeout:      50 * time.Second,
+		Timeout:      120 * time.Second,
 		PollInterval: 500 * time.Millisecond,
 		EncodeBase32: true,
 	}
